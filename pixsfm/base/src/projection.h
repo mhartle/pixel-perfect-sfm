d1829668e99c904f6231e49688ac12ee24640728
Michael Hartle <mhartle@hartle-klug.com>
2024-05-31 16:49:44 +0200

    Migrating to colmap 2.*

--- pixsfm/base/src/projection.h
+++ pixsfm/base/src/projection.h
@@ -42,7 +42,7 @@ template <typename CameraModel, typename T>
 void PixelToWorld(const T* camera_params, const T* qvec, const T* tvec,
                   const T x, const T y, const T* depth, T* xyz) {
   T local_xyz[3];
-  UndistortionAutodiffModel<CameraModel>::ImageToWorld(
+  UndistortionAutodiffModel<CameraModel>::CamFromImage(
       camera_params, x, y, &local_xyz[0], &local_xyz[1]);
 
   local_xyz[2] = T(1.0);
@@ -70,7 +70,7 @@ inline void WorldToPixel(const T* camera_params, const T* qvec, const T* tvec,
   projection[0] /= projection[2];  // u
   projection[1] /= projection[2];  // v
 
-  CameraModel::WorldToImage(camera_params, projection[0], projection[1], &xy[0],
+  CameraModel::ImageFromCam(camera_params, projection[0], projection[1], &xy[0],
                             &xy[1]);
 }
 
